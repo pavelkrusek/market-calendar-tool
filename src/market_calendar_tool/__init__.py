2a64388c7b1e7e57682fbb997059add63a00604a
Pavel Krusek <pavel@krusek.dk>
2024-10-15 17:49:42 +0200

    feat: Update init files

--- src/market_calendar_tool/__init__.py
+++ src/market_calendar_tool/__init__.py
@@ -1,3 +1,4 @@
-from .scraper import BaseScraper
+from .api import scrape_calendar
+from .scraper.constants import Site
 
-__all__ = ["BaseScraper"]
+__all__ = ["scrape_calendar", "Site"]
--- src/market_calendar_tool/scraper/__init__.py
+++ src/market_calendar_tool/scraper/__init__.py
@@ -1,3 +1,4 @@
 from .base_scraper import BaseScraper
+from .constants import Site, site_number_mapping
 
-__all__ = ["BaseScraper"]
+__all__ = ["BaseScraper", "Site", "site_number_mapping"]
