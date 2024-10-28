aba53c4769aef372726dda33ff753e323c46724d
Pavel Krusek <pavel@krusek.dk>
2024-10-28 18:45:59 +0100

    feat: Add a whole object save to the pickle

--- src/market_calendar_tool/mixins/save_mixin.py
+++ src/market_calendar_tool/mixins/save_mixin.py
@@ -1,4 +1,5 @@
 import os
+import pickle
 from enum import Enum
 from typing import Optional
 
@@ -14,29 +15,34 @@ class SaveFormat(Enum):
 class SaveMixin:
     def save(
         self,
+        output_dir: Optional[str] = None,
+        file_name: Optional[str] = None,
+    ):
+        output_dir = self._ensure_output_dir(output_dir)
+
+        if file_name is None:
+            file_name = "data.pickle"
+            logger.warning("No file_name provided. Using default 'data.pickle'.")
+
+        file_path = os.path.join(output_dir, file_name)
+
+        try:
+            with open(file_path, "wb") as f:
+                pickle.dump(self, f)
+            logger.info(f"Serialized ScrapeResult object to '{file_path}'.")
+        except Exception as e:
+            logger.error(
+                f"Failed to serialize ScrapeResult object to '{file_path}': {e}"
+            )
+            raise e
+
+    def save_to_dataframes(
+        self,
         save_format: SaveFormat = SaveFormat.PARQUET,
         output_dir: Optional[str] = None,
         file_prefix: str = "data",
     ):
-        if output_dir is None:
-            output_dir = os.getcwd()
-            logger.info(
-                f"No output_dir provided. Using current working directory: {output_dir}"
-            )
-        else:
-            if not os.path.exists(output_dir):
-                try:
-                    os.makedirs(output_dir)
-                    logger.info(f"Created output directory: {output_dir}")
-                except OSError as e:
-                    logger.error(
-                        f"Failed to create output directory '{output_dir}': {e}"
-                    )
-                    raise OSError(
-                        f"Failed to create output directory '{output_dir}': {e}"
-                    )
-            else:
-                logger.info(f"Using existing output directory: {output_dir}")
+        output_dir = self._ensure_output_dir(output_dir)
 
         for attribute_name, attribute_value in self.__dict__.items():
             if isinstance(attribute_value, pd.DataFrame):
@@ -56,3 +62,36 @@ class SaveMixin:
                         f"Failed to save '{attribute_name}' DataFrame to '{file_path}': {e}"
                     )
                     raise e
+
+    @staticmethod
+    def load_object(file_path: str):
+        try:
+            with open(file_path, "rb") as f:
+                obj = pickle.load(f)
+            logger.info(f"Deserialized object from '{file_path}'.")
+            return obj
+        except Exception as e:
+            logger.error(f"Failed to deserialize object from '{file_path}': {e}")
+            raise e
+
+    def _ensure_output_dir(self, output_dir: Optional[str]) -> str:
+        if output_dir is None:
+            output_dir = os.getcwd()
+            logger.info(
+                f"No output_dir provided. Using current working directory: {output_dir}"
+            )
+        else:
+            if not os.path.exists(output_dir):
+                try:
+                    os.makedirs(output_dir)
+                    logger.info(f"Created output directory: {output_dir}")
+                except OSError as e:
+                    logger.error(
+                        f"Failed to create output directory '{output_dir}': {e}"
+                    )
+                    raise OSError(
+                        f"Failed to create output directory '{output_dir}': {e}"
+                    )
+            else:
+                logger.info(f"Using existing output directory: {output_dir}")
+        return output_dir
